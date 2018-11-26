acef6dbf8ea70cc92086b3341d40f0d369e8c4bf
VidhyaKN24 <45349827+VidhyaKN24@users.noreply.github.com>
2018-11-26 14:50:18 +0530

    Add files via upload

---
+++ SMSSend.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SMSSend.aspx.cs" Inherits="Sampradana_SMSSending.SMSSend" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="txtMessage" runat="server" >
+
+        </asp:TextBox>
+        <asp:Button ID="btnsend" runat="server" Text="Send" OnClick="btnsend_Click" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ SMSSend.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TextmagicRest.Model;
+using TextmagicRest;
+
+namespace Sampradana_SMSSending
+{
+    public partial class SMSSend : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnsend_Click(object sender, EventArgs e)
+        {
+           
+
+            var client = new Client("test", "my-api-key");
+            var link = client.SendMessage("Hello from TextMagic API", "8123131426");
+            if (link.Success)
+            {
+                Console.WriteLine("Message ID {0} has been successfully sent", link.Id);
+            }
+            else
+            {
+                Console.WriteLine("Message was not sent due to following exception: {0}", link.ClientException.Message);
+           }
+        }
+    }
+}
